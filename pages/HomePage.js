1a02c78128086c808b2084483807d6ff7d76f108
4lph4shell <4lph4ir@gmail.com>
2024-09-23 18:13:13 +0330

    command

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,6 +1,13 @@
+/**
+ * Function to render the home page
+ * @returns {String} The html for the home page
+ */
 const HomePage = () => {
   return `
   <p>
+  <!-- This is the home page of a simple SPA written in JavaScript -->
+  <!-- It does not use any libraries or frameworks and uses the -->
+  <!-- functional programming paradigm -->
   این یک برنامه SPA است که با زبان جاوااسکریپت توسعه داده شده. در فرآیند توسعه از کتابخانه‌ها و یا چهارچوب‌ها استفاده نشده و با پارادایم برنامه نویسی تابعی نرم افزار را پیاده سازی کرده‌ایم.
   </p>
   `
